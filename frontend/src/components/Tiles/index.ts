80169635a2f2a2e93e1c029577fdea22b3f94ab5
SathyaDynpro <sathyaprakash.j@dynpro.in>
2025-10-28 11:24:15 +0530

    Delete frontend directory

--- frontend/src/components/Tiles/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TileCreator } from './TileCreator';
-export { default as TileEditor } from './TileEditor';
-export { default as TileComponent } from './TileComponent';
-export type { CreateTileDto } from './TileCreator';
-export type { TileData } from './TileComponent';
